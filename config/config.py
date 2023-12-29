8b66e24e090de23ccd258b868a6497e0537ba8de
Huy Dương <duongquanghuy2792000@gmail.com>
2023-12-29 15:02:23 +0700

    Change mongo remote

--- config/config.py
+++ config/config.py
@@ -1,21 +1,21 @@
 import os
+import ssl
 from typing import Optional
 
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 import models as models
 
-
 async def initiate_database():
     print("====initiate_database====")
-    user = os.getenv('MONGODB_USERNAME')
-    password = os.getenv('MONGODB_PASSWORD')
-    host = os.getenv('MONGODB_HOST')
-    port = int(os.getenv('MONGODB_PORT', 27017))
+    user = os.getenv('MONGODB_USERNAME', 'ltp')
+    password = os.getenv('MONGODB_PASSWORD', 'password')
+    host = os.getenv('MONGODB_HOST', 'localhost')
     db_name = os.getenv('MONGODB_NAME', 'ltp')
 
-    DATABASE_URL = f"mongodb://{user}:{password}@{host}:{port}/{db_name}"
+    DATABASE_URL = f"mongodb+srv://{user}:{password}@{host}/"
     client = AsyncIOMotorClient(DATABASE_URL)
+    print(client)
     await init_beanie(
         database=client[db_name], document_models=models.__all__
     )
